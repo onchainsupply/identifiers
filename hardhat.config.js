3e99b7f2547e2cd2ba4b69faef97881d1f172bc8
raldblox <raldblox@outlook.com>
2025-02-06 05:28:25 +0800

    config

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    etherlinkMainnet: {
+      url: "https://node.mainnet.etherlink.com",
+      accounts: [process.env.OCS_PRIVATE_KEY],
+    },
+    etherlinkTestnet: {
+      url: "https://node.ghostnet.etherlink.com",
+      accounts: [process.env.OCS_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      etherlinkMainnet: "abc",
+      etherlinkTestnet: "abc",
+    },
+    customChains: [
+      {
+        network: "etherlinkMainnet",
+        chainId: 42793,
+        urls: {
+          apiURL: "https://explorer.etherlink.com/api",
+          browserURL: "https://explorer.etherlink.com",
+        },
+      },
+      {
+        network: "etherlinkTestnet",
+        chainId: 128123,
+        urls: {
+          apiURL: "https://testnet.explorer.etherlink.com/api",
+          browserURL: "https://testnet.explorer.etherlink.com",
+        },
+      },
+    ],
+  },
+};
